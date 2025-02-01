852cdf2998feb8ba193021110b7516eabb1078cc
Elise Kristiansen <elise.kri@hotmail.com>
2025-02-01 16:48:26 +0100

    Added basic wagtail

---
+++ frkfrontend/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-9p#5)orjok+2n0&_zd_gsnw)q9_s*n9*pkq&m5s1clme!w4yja"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
